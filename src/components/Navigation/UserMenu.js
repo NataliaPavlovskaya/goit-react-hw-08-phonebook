c84862bc1871c05fa386431900fef23b43720a1a
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-09-08 21:13:15 +0300

    css

--- src/components/Navigation/UserMenu.js
+++ src/components/Navigation/UserMenu.js
@@ -2,19 +2,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getUserName } from '../../redux/auth/auth-selectors';
 import authOperations from '../../redux/auth/auth-operations';
 import { ImExit } from 'react-icons/im';
-import s from './Navigations.module.css';
+import styles from './Navigations.module.css';
 
 export default function UserMenu() {
   const name = useSelector(getUserName);
   const dispatch = useDispatch();
 
   return (
-    <div className={s.userWrapper}>
-      <span className={s.name}>{name}</span>
+    <div className={styles.userWrapper}>
+      <span className={styles.name}>{name}</span>
 
       <ImExit
         size="20"
-        className={s.button}
+        className={styles.button}
         type="button"
         onClick={() => dispatch(authOperations.logOut())}
       />
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,16 +1,16 @@
 import { NavLink } from 'react-router-dom';
 import Section from '../components/Layout/Section';
 import Container from '../components/Layout/Container';
-import s from './HomeView.module.css';
+import styles from './HomeView.module.css';
 
 export default function HomeView() {
   return (
     <Container>
-      <Section className={s.section}>
-        <div className={s.wrapper}>
-          <h1 className={s.title}>Добро пожаловать!</h1>
-          <NavLink className={s.link} to="/register"/>
-          <p className={s.description}>
+      <Section className={styles.section}>
+        <div className={styles.wrapper}>
+          <h1 className={styles.title}>Добро пожаловать!</h1>
+          <NavLink className={styles.link} to="/register"/>
+          <p className={styles.description}>
             Приложение для записи и хранения телефонных номеров ваших друзей
           </p>
         </div>
