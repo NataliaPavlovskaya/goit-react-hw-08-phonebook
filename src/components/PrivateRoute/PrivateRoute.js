ef9b9ddb7fdd61aa596e3a92ad5a76f5f1e93211
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-09-12 11:28:58 +0300

    good job

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,26 +1,15 @@
 import { useSelector } from 'react-redux';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { getIsAuthenticated } from '../../redux/auth/auth-selectors';
 
-export default function PrivateRoute({
+export default function PublicRoute({
+  restricted = false,
+  redirectTo = '/',
   component: Component,
-  redirectTo,
-  ...routeProps
 }) {
   const isAuthenticated = useSelector(getIsAuthenticated);
 
-  return (
-    <Routes>
-    <Route
-      {...routeProps}
-      render={props =>
-        isAuthenticated ? (
-          <Component {...props} />
-        ) : (
-          <Navigate to={redirectTo} />
-        )
-      }
-    />
-    </Routes>
-  );
+  const shouldRedirect = isAuthenticated && restricted;
+  return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
+
 }
